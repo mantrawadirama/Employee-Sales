ecf0443af0f37eab4c1c7c3c03bf999485d47961
ramakrishna <mantrawadirama@gmail.com>
2020-03-08 22:49:43 -0500

    Base version of singleton in logger.

--- Employee.Logging/Logger.cs
+++ Employee.Logging/Logger.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Employee.Logging
 {
-    public class Logger
+    public sealed class Logger
     {
+        // This is Base version of Singleton.
+        // code deosn't support multi-threaded aplication
+        private static Logger instance = null;
+        private static Logger GetInstance
+        {
+            get
+            {
+                if (instance == null)                
+                    instance = new Logger();
+                return instance;                
+            }
+        }
+        private Logger()
+        {
+
+        }
     }
 }
