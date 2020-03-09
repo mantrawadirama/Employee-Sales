af367637ea09f7ee7fd4896c1b1c7715e8d1eddc
ramakrishna <mantrawadirama@gmail.com>
2020-03-08 23:05:36 -0500

    updated version for singleton - lgger

--- Employee.Logging/Logger.cs
+++ Employee.Logging/Logger.cs
@@ -6,20 +6,33 @@ using System.Threading.Tasks;
 
 namespace Employee.Logging
 {
+    //sealed class to avoid inheritence which also helps not to create more than 1 object
     public sealed class Logger
     {
         // This is Base version of Singleton.
         // code deosn't support multi-threaded aplication
         private static Logger instance = null;
+        private static readonly object obj = new object();
+        // Property by which an instance can be created
         private static Logger GetInstance
         {
             get
             {
-                if (instance == null)                
-                    instance = new Logger();
+             // As lock is costly check for instance to be null again before entering lock. This is called double check lock
+                if (instance == null)
+                {
+                    //lock will help us only 1 thread can enter at any given point
+                    // if 2 controllers need to log lock will help until other action is compeleted
+                    lock (obj)
+                    {
+                        if (instance == null)
+                            instance = new Logger();
+                    }
+                }
                 return instance;                
             }
         }
+        // Restricted object creation 
         private Logger()
         {
 
